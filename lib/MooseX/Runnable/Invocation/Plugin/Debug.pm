00fc26d5a389e344af1cf6d54dd779e6578bac4d
Jonathan Rockway <jon@jrock.us>
2009-04-29 09:54:29 -0500

    actually print args in Debug plugin

--- lib/MooseX/Runnable/Invocation/Plugin/Debug.pm
+++ lib/MooseX/Runnable/Invocation/Plugin/Debug.pm
@@ -1,19 +1,19 @@
 package MooseX::Runnable::Invocation::Plugin::Debug;
 use Moose::Role;
-use Context::Preserve qw(preserve_context);
 
-for my $method (qw/load_class apply_scheme validate_class create_instance start_application/){
-
+for my $method (qw{
+    load_class apply_scheme validate_class
+    create_instance start_application
+  }){
     requires $method;
 
     before $method => sub {
-        my ($self, @args);
+        my ($self, @args) = @_;
         my $args = join ', ', @args;
         print "Calling $method($args)\n";
     };
 
     after $method => sub {
-        my ($next, $self, @args) = @_;
         print "Returning from $method\n";
     };
 }
