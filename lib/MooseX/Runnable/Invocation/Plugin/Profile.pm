d068787919a9e1a12181cab62e7db915818b9ffd
Jonathan Rockway <jon@jrock.us>
2009-06-25 14:42:08 -0700

    add prototype Profile plugin

---
+++ lib/MooseX/Runnable/Invocation/Plugin/Profile.pm
@@ -0,0 +1,21 @@
+package MooseX::Runnable::Invocation::Plugin::Profile;
+use Moose::Role;
+
+before 'load_class' => sub {
+    my ($self) = @_;
+    confess 'The Profile plugin cannot be used when not invoked via mx-urn'
+      unless $self->does('MooseX::Runnable::Invocation::Role::WithParsedArgs');
+
+    my @cmdline = $self->parsed_args->guess_cmdline(
+        perl_flags      => ['-d:NYTProf'],
+        without_plugins => ['Profile', '+'.__PACKAGE__],
+    );
+
+    eval { $self->_debug_message(
+        "Re-execing with ". join ' ' , @cmdline,
+    )};
+
+    exec(@cmdline);
+};
+
+1;
